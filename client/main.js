bfa98c40b559736c96f580b6f4719ed19ae353a2
KORE-CORP\gbaez <GBaez@korewireless.com>
2018-09-17 12:55:35 -0400

    Adding Router, Material UI And Home component

---
+++ client/main.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles'
+import {indigo, pink} from 'material-ui/colors'
+  
+render(<App/>, document.getElementById('root'))
--- webpack.config.client.js
+++ webpack.config.client.js
@@ -3,14 +3,18 @@ const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd()
 
 const config = {
-    mode: "production",
+    name: "browser",
+    mode: "development",
+    devtool: 'eval-source-map',
     entry: [
+        'react-hot-loader/patch',
+        'webpack-hot-middleware/client?reload=true',
         path.join(CURRENT_WORKING_DIR, 'client/main.js')
     ],
     output: {
         path: path.join(CURRENT_WORKING_DIR , '/dist'),
         filename: 'bundle.js',
-        publicPath: "/dist/"
+        publicPath: '/dist/'
     },
     module: {
         rules: [
@@ -20,9 +24,16 @@ const config = {
                 use: [
                     'babel-loader'
                 ]
+            },
+            {
+                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+                use: 'file-loader'
             }
         ]
-    }
+    },  plugins: [
+          new webpack.HotModuleReplacementPlugin(),
+          new webpack.NoEmitOnErrorsPlugin()
+      ]
 }
 
 module.exports = config
