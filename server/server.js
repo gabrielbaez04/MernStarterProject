3a9bba0d2a26b92010e59791db1ed75ddbc39e32
gabrielbaez04 <gabrielbaez04@gmail.com>
2018-09-16 23:05:21 -0400

    Configure Mongo on serverjs and start adding config for ui implementation.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 import config from './../config/config'
 import app from './express'
+import mongoose from 'mongoose'
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri)
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`)
+})
 
 app.listen(config.port, (err) => {
   if (err) {
